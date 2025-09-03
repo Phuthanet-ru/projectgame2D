9efb9c6c351b31a01b5ab576aa1833264f295e20
aquamarine189 <akerat48.ky@gmail.com>
2025-09-04 02:24:50 +0700

    Delete Scenes/Levels directory

--- Scenes/Levels/Scenes/Prefabs/carpetnextlevel.gd
+++
@@ -1,8 +0,0 @@
-# Door.gd
-extends Area2D
-
-@export var next_scene : PackedScene
-
-func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("Player"):
-		SceneTransition.load_scene(next_scene)
--- Scenes/Levels/mainmenu.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _on_Start_Game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/ห้องนอน.tscn") # เปลี่ยนเป็นพาธฉากแรกของคุณ
-
-func _on_Quit_Game_button_pressed() -> void:
-	get_tree().quit()
